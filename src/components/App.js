b1376d8d66af89023c0db1a36ba41d760db3d476
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-04-23 11:38:45 +0200

    Render header component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./header/Header";
+
+const App = () => (
+  <Router>
+    <Header />
+  </Router>
+)
+
+export default App;
