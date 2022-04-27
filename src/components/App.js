3c175d660b7be4bd40755bba6d3d2b6d5da7be05
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-04-27 15:32:00 +0200

    Render home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./header/Header";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
 
 const App = () => (
   <Router>
     <Header />
+    <Home />
   </Router>
 )
 
