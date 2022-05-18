0a017e261e8dfebce5ebf2ca3a705cd622d4243c
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-05-18 19:26:05 +0200

    Solve build errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
-import Resume from "./Resume/Resume";
 
 const App = () => (
     <BrowserRouter basename="/">
       <Routes>
         <Route element={<Home />} path="/" />
-        <Route element={<Resume />} path="/resume.pdf" />
       </Routes>
     </BrowserRouter>
 );
